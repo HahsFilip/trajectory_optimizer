67c86d848f315bd7e5065678b7834bb03e790353
HahsFilip <filip.hahs@gmail.com>
2023-04-18 15:21:14 +0200

    working motion

--- src/main.rs
+++ src/main.rs
@@ -27,20 +27,9 @@ fn calculate_accelerations(planet_masses: Vec<f32>, planet_location: Vec<[f32;2]
     }
     result
 }
-fn f(state: State, planet_masses: Vec<f32>, planet_location: Vec<[f32;2]>,  g_number: f32) -> State{
-    
-    let acceleration =     calculate_accelerations(planet_masses.clone(), planet_location.clone(), [state.x, state.y], g_number);
-
-    State {
-        x: state.vx,
-        y: state.vy,
-        vx:  acceleration[0],
-        vy: acceleration[1],
-    }
-}
 
 
-fn rk4<F: Fn(State, Vec<f32>,Vec<[f32;2]>, f32) -> State>(
+fn integrate<F: Fn( Vec<f32>, Vec<[f32;2]>,[f32;2], f32) ->[f32;2] >(
     f: F,
     state: State,
    
@@ -49,32 +38,12 @@ fn rk4<F: Fn(State, Vec<f32>,Vec<[f32;2]>, f32) -> State>(
     planet_masses: Vec<f32>,
     g:f32
 ) -> State {
-    let state_2 = state;
-    let k1 = f(state_2, planet_masses.clone(), planet_location.clone(),g);
-    let k2 = f(State {
-        x: state.x + 0.5*k1.x,
-        y: state.y + 0.5*k1.y,
-        vx: state.vx + 0.5*k1.vx,
-        vy: state.vy + 0.5*k1.vy,
-    }, planet_masses.clone(), planet_location.clone(),g);
-    let k3 = f(State {
-        x: state.x + 0.5*k2.x,
-        y: state.y + 0.5*k2.y,
-        vx: state.vx + 0.5*k2.vx,
-        vy: state.vy + 0.5*k2.vy,
-    }, planet_masses.clone(), planet_location.clone(),g);
-    let k4 = f(State {
-        x: state.x + k3.x,
-        y: state.y + k3.y,
-        vx: state.vx + k3.vx,
-        vy: state.vy + k3.vy,
-    }, planet_masses.clone(), planet_location.clone(),g);
-
+    let acc = f(planet_masses, planet_location, [state.x, state.y],g);
     State {
-        x: state.x + dt*(k1.x + 2.0*k2.x + 2.0*k3.x + k4.x)/6.0,
-        y: state.y + dt*(k1.y + 2.0*k2.y + 2.0*k3.y + k4.y)/6.0,
-        vx: state.vx + dt*(k1.vx + 2.0*k2.vx + 2.0*k3.vx + k4.vx)/6.0,
-        vy: state.vy + dt*(k1.vy + 2.0*k2.vy + 2.0*k3.vy + k4.vy)/6.0,
+        x: state.x + dt*state.vx,
+        y: state.y + dt*state.vy,
+        vx: state.vx + dt*acc[0],
+        vy: state.vy + dt*acc[1],
     }
 }
 
@@ -94,14 +63,14 @@ impl Clone for State {
 }
 
 fn main() -> Result<(), String> {
-    let planet_masses = vec![1.0;1];
+    let planet_masses = vec![10.0;1];
     let planet_location = vec![[0.0, 0.0];1];
-    let g_const = 1.0;
+    let g_const = 1000.0;
 
     
-    let n_time_steps = 10000;
-    let dt = 0.00001;
-    let mut state = State{x:1.0, y: 0.0, vx:0.0, vy:0.1 };
+    let n_time_steps = 100000;
+    let dt = 0.001;
+    let mut state = State{x:100.0, y: 0.0, vx:0.0, vy:5.0};
     let mut velocities = vec![[0.0,0.0];n_time_steps];
     let mut positions = vec![[0.0,0.0];n_time_steps];
     
@@ -124,14 +93,13 @@ fn main() -> Result<(), String> {
     for i in 0..n_time_steps{
         canvas.set_draw_color(Color::RGB(0, 0, 0));
 
-        canvas.clear();
-        state = rk4(f, state, dt, planet_location.clone(), planet_masses.clone(), g_const);
-        println!("{} {} {}",(state.x as i32), state.vy, state.x.powf(2.0)+state.y.powf(2.0));
+        state = integrate(calculate_accelerations, state, dt, planet_location.clone(), planet_masses.clone(), g_const);
+        println!("{} {} {}",(state.x*10.0), (state.y*10.0) as i32, state.x.powf(2.0)+state.y.powf(2.0));
         canvas.set_draw_color(Color::RGB(100, 100, 100));
 
-        canvas.fill_rect(Rect::new((state.x as i32)*100+400,(state.y as i32)*100+300 as i32, 10, 10) );
+        canvas.fill_rect(Rect::new((state.x+400.0) as i32,(state.y +300.0)as i32, 10, 10) );
         canvas.present();
-        
+
     }
     
 
