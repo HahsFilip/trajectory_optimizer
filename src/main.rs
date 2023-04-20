8984668af8ab436b6ce032b90249e9f12d7b6f43
FilipHahs <F.Hahs@quickersim.com>
2023-04-20 08:17:52 +0200

    rk4 works

--- src/main.rs
+++ src/main.rs
@@ -39,26 +39,41 @@ fn integrate<F: Fn( Vec<f32>, Vec<[f32;2]>,[f32;2], f32) ->[f32;2] >(
     g:f32
 ) -> State {
     //let acc = f(planet_masses, planet_location, [state.x, state.y],g);
-    let mut k1 = f(planet_masses.clone(), planet_location.clone(), [state.x, state.y],g);
-    let mut k2 = f(planet_masses.clone(), planet_location.clone(), [state.x+ dt*k1[0]/2.0, state.y + dt*k1[1]/2.0],g);
-    let mut k3 = f(planet_masses.clone(), planet_location.clone(), [state.x+ dt*k2[0]/2.0, state.y + dt*k2[1]/2.0],g);
-    let mut k4 = f(planet_masses.clone(), planet_location.clone(), [state.x+ dt*k3[0], state.y + dt*k3[1]],g);
-    let mut acc = [0.0;2];
-    acc = [(k1[0]+ 2.0*k2[0]+2.0*k3[0]+ k4[0])/6.0,(k1[1]+ 2.0*k2[1]+2.0*k3[1]+ k4[1])/6.0];
-    k1 = [state.vx, state.vy];
-    k2 =  [state.vx+ dt*k1[0]/2.0, state.vy + dt*k1[1]/2.0];
-    k3 =[state.vx+ dt*k2[0]/2.0, state.vy + dt*k2[1]/2.0];
-    k4 = [state.vx+ dt*k3[0], state.vy + dt*k3[1]];
+    // let mut k1 = f(planet_masses.clone(), planet_location.clone(), [state.x, state.y],g);
+    // let mut k2 = f(planet_masses.clone(), planet_location.clone(), [state.x+ dt*k1[0]/2.0, state.y + dt*k1[1]/2.0],g);
+    // let mut k3 = f(planet_masses.clone(), planet_location.clone(), [state.x+ dt*k2[0]/2.0, state.y + dt*k2[1]/2.0],g);
+    // let mut k4 = f(planet_masses.clone(), planet_location.clone(), [state.x+ dt*k3[0], state.y + dt*k3[1]],g);
+    // let mut acc = [0.0;2];
+    // acc = [(k1[0]+ 2.0*k2[0]+2.0*k3[0]+ k4[0])/6.0,(k1[1]+ 2.0*k2[1]+2.0*k3[1]+ k4[1])/6.0];
+    // k1 = [state.vx, state.vy];
+    // k2 =  [state.vx+ dt*k1[0]/2.0, state.vy + dt*k1[1]/2.0];
+    // k3 =[state.vx+ dt*k2[0]/2.0, state.vy + dt*k2[1]/2.0];
+    // k4 = [state.vx+ dt*k3[0], state.vy + dt*k3[1]];
 
-    let mut vel = [0.0; 2];
+    // let mut vel = [0.0; 2];
     
-    vel  = [(k1[0]+ 2.0*k2[0]+2.0*k3[0]+ k4[0])/6.0,(k1[1]+ 2.0*k2[1]+2.0*k3[1]+ k4[1])/6.0];
-
+    // vel  = [(k1[0]+ 2.0*k2[0]+2.0*k3[0]+ k4[0])/6.0,(k1[1]+ 2.0*k2[1]+2.0*k3[1]+ k4[1])/6.0];
+    let mut a = f(planet_masses.clone(), planet_location.clone(), [state.x, state.y],g);
+    a[0] = dt*a[0]/2.0;
+    a[1] = dt*a[1]/2.0;
+    let beta = [dt*(state.vx+a[0]/2.0)/2.0, dt*(state.vy+a[1]/2.0)/2.0];
+    let mut b = f(planet_masses.clone(), planet_location.clone(), [state.x+beta[0], state.y+beta[1]],g);
+    b[0] = dt*b[0]/2.0;
+    b[1] = dt*b[1]/2.0;
+    let mut c = f(planet_masses.clone(), planet_location.clone(), [state.x+beta[0], state.y+beta[1]],g);
+    c[0] = dt*c[0]/2.0;
+    c[1] = dt*c[1]/2.0;
+    let delta = [dt*(state.vx+c[0]),dt*(state.vy+c[1])];
+    let mut d = f(planet_masses.clone(), planet_location.clone(), [state.x+delta[0], state.y+delta[1]],g);
+    d[0] = dt*d[0]/2.0;
+    d[1] = dt*d[1]/2.0;
+    let k = [(a[0] + b[0] + c[0])/3.0,(a[1] + b[1] +c[1])/3.0];
+    let j = [(a[0] + 2.0*b[0] + 2.0*c[0] + d[0])/3.0, (a[1] + 2.0*b[1] + 2.0*c[1] + d[1])/3.0];
     State {
-        x: state.x + dt*vel[0],
-        y: state.y + dt*vel[1],
-        vx: state.vx + dt*acc[0],
-        vy: state.vy + dt*acc[1],
+        x: state.x + dt*(state.vx+k[0]),
+        y: state.y + dt*(state.vy+k[1]),
+        vx: state.vx + j[0],
+        vy: state.vy + j[1],
     }
 }
 
@@ -84,8 +99,8 @@ fn main() -> Result<(), String> {
     let g_const = 1000.0;
 
     
-    let n_time_steps = 1000000;
-    let dt = 0.0001;
+    let n_time_steps = 100000;
+    let dt = 0.001;
     let mut state = State{x:100.0, y: 0.0, vx:0.0, vy:5.0};
     let mut velocities = vec![[0.0,0.0];n_time_steps];
     let mut positions = vec![[0.0,0.0];n_time_steps];
@@ -114,7 +129,7 @@ fn main() -> Result<(), String> {
         canvas.set_draw_color(Color::RGB(100, 100, 100));
 
         canvas.fill_rect(Rect::new((state.x+400.0) as i32,(state.y +300.0)as i32, 1, 1) );
-        if i%10000 == 0{
+        if i%1000 == 0{
             canvas.present();
 
         }
